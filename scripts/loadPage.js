59baf436d146a28b28d2ded9bda4895328c32600
seong954t <seomtae@naver.com>
2018-05-11 23:31:23 +0900

    loadPage update

--- scripts/loadPage.js
+++ scripts/loadPage.js
@@ -1,5 +1,21 @@
 var rootURL = "http://localhost/catch-us/";
+
 function onLoadSignUpPage(){
     location.href = rootURL+"pages/signup.html";
 }
 
+function onLoadClitentPage(){
+    location.href = rootURL+"pages/client-main.html";
+}
+
+function onLoadExpertPage(){
+    location.href = rootURL+"pages/expert-main.html";
+}
+
+function onLoadManagertPage(){
+    location.href = rootURL+"pages/manager.html";
+}
+
+function onLoadBoardPage(){
+    location.href = rootURL+"pages/board.html";
+}
