20a2704f52f0027a25ab1776d68b09566376b8f2
seong954t <seomtae@naver.com>
2018-05-14 18:14:44 +0900

    add user estimates load function

--- scripts/getFirebase.js
+++ scripts/getFirebase.js
@@ -1,6 +1,6 @@
 
 function getExpertList(){
-    var UserList = firebase.database().ref("/User");
+    var UserList = firebase.database().ref("/Users");
     return UserList.orderByChild("personalInfo/type").equalTo("Expert").once('value')
     // .then(
     //     function(snapshot){
---
+++ scripts/manage.js
@@ -0,0 +1,16 @@
+var EstimatesList = {}
+function getEstimateList(){
+    var UserList = firebase.database().ref("/Users");
+    return UserList.once('value').then(
+        function(snapshot){
+            for(row of Object.values(snapshot.val())){
+                if(row["Estimates"] != null){
+                    $.extend(EstimatesList, row["Estimates"]);
+                }
+            }
+            console.log(EstimatesList)
+        }
+    )
+}
+
+getstimateList();
